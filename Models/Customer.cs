e0978b6020e155e660900d356737bef006ff48be
Nero <nirar1992@gmail.com>
2026-02-27 17:19:39 +0100

    Created customer class

---
+++ Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HotelApp.Models
+
+{
+   
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+
+        // Konstruktor
+        public Customer(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            PhoneNumber = phone;
+        }
+       
+        public Customer()
+        {
+        }
+    }
+}
