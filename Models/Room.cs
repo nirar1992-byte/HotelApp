14c842998416b3eac64f653a5b9fb3c12eb5596b
Nero <nirar1992@gmail.com>
2026-02-27 17:27:45 +0100

    Created Room class

---
+++ Models/Room.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Models
+{
+    public class Room
+    {
+        //Properties
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public string RoomType { get; set; }
+        public int ExtraBeds { get; set; }
+        public bool IsAvailable { get; set; }
+        public List <Booking> Bookings { get; set; } = new List<Booking>();
+
+
+
+        //Konstruktor
+        public Room(string roomName,string roomType, int extraBeds)
+        {
+           RoomName = roomName;
+            RoomType = roomType;
+            ExtraBeds = extraBeds;
+            IsAvailable = true;
+        }
+
+        // Databas konstruktor
+        public Room() 
+        {
+        }
+
+
+
+    }
+}
