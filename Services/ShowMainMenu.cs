8dc6eea026dbc7062916b7c0dfdb6d5e0b7fd78b
Nero <nirar1992@gmail.com>
2026-02-28 17:15:28 +0100

    Created last method

---
+++ Services/ShowMainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelApp.Services
+{
+    public class ShowMainMenu
+    {
+        public static void ShowMainMenuMethod()
+        {
+            Console.Clear();
+            Console.WriteLine("======================================");
+            Console.WriteLine("|\t                             |");
+            Console.WriteLine("|\t1. Hantera Rum               |");
+            Console.WriteLine("|\t2. Hantera Kunder            |");
+            Console.WriteLine("|\t3. Hantera Bokningar         |");
+            Console.WriteLine("|\t4. Visa Tillgänglighet       |");
+            Console.WriteLine("|\t5. Avsluta Program           |");
+            Console.WriteLine("|\t                             |");
+            Console.WriteLine("======================================");
+            Console.WriteLine();
+        }
+
+    }
+}
