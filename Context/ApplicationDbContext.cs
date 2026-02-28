9b30797e6f1445328e3ccdb3343dc66a54747dca
Nero <nirar1992@gmail.com>
2026-02-28 14:29:02 +0100

    started with creating dbcontext

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HotelApp.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Models.Customer> Customers { get; set; }
+        public DbSet<Models.Room> Rooms { get; set; }
+        public DbSet<Models.Booking> Bookings { get; set; }
+
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
