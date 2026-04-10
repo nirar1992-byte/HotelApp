c43687562ea0f6d8c618145ffd0c164d661ef529
nirar-1992byte <nirar1992@gmail.com>
2026-04-10 15:49:05 +0200

    .

--- Program.cs
+++ Program.cs
@@ -33,14 +33,14 @@ namespace HotelApp
             // Skapa HotelManager
             HotelManager hotelManager = new HotelManager(dbContext);
 
-            // ✅👇 LÄGG TILL DETTA BLOCK HÄR
+            // LÄGG TILL DETTA BLOCK HÄR
             var sqlService = new SqlService();
 
             sqlService.GetAllRooms();
             sqlService.GetAvailableRooms();
             sqlService.CountRooms();
             sqlService.GetRoomBookings();
-            // ✅👆 SLUT PÅ DITT TILLÄGG
+            // SLUT PÅ DITT TILLÄGG
 
             bool runProgram = true;
 
